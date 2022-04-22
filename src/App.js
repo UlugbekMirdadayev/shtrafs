56bd749effce5c657d512c63190fe9cffbc44a09
Ulugbek Mirdadayev <ulugbekmirdadayev1211@gmail.com>
2022-04-22 17:36:27 +0500

    update public/index.html, App.js and src/index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function App () {
-  return (
-    <div className="app">
-     
-    </div>
-  );
-};
+function App() {
+  return <div className="app"></div>;
+}
 
 export default App;
