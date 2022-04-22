4967a0491873599ed4bdc8ae3901a59f4559a677
Ulugbek Mirdadayev <ulugbekmirdadayev1211@gmail.com>
2022-04-22 20:14:51 +0500

    build(deps): update package-lock.json, package.json, App.js and src/index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<>Home Page</>} />
+        <Route path="/shtraf/:id" element={<>Shtraf Page</>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
